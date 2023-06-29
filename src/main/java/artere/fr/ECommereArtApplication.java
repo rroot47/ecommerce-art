2f97cb4fd781f5d655337ec84831cdf27f0a39ce
bsylla-art <bsylle@artere-cs.com>
2023-06-29 15:11:11 +0200

    add project first commit

---
+++ src/main/java/artere/fr/ECommereArtApplication.java
@@ -0,0 +1,13 @@
+package artere.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommereArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommereArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/artere/fr/ECommereArtApplicationTests.java
@@ -0,0 +1,13 @@
+package artere.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommereArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
